beaf9d662828086025d53db22c1826d9b8ee7fae
mbenzan <moisesbenzan@outlook.com>
2017-04-13 15:44:35 -0400

    Codigo limpiado y convertido a clase en YeezySupply.py

--- bottilcart.py
+++ bottilcart.py
@@ -2,59 +2,43 @@ import requests
 from bs4 import BeautifulSoup as soup
 import re
 
-
-ming= "https://yeezysupply.com/cart/add.js"
+ming = "https://yeezysupply.com/cart/add.js"
 ing0 = "https://yeezysupply.com/"
-ing1= "https://yeezysupply.com/collections/"
+ing1 = "https://yeezysupply.com/collections/"
 
-def secondtest3b(url, tags):
-          r = requests.get(url)
-          s = soup(r.content, 'html.parser')
-          t = s.find_all(tags)
-          u = str(t[2])
-          v = (re.findall(r'[a-z]*1', u))
-          return v[0]
-          
-def test5(url, tags):
-         r = requests.get(url)
-         souping = soup(r.content, 'html.parser')
-         return souping.find_all(tags)
 
-ing2 = secondtest3b("http://yeezysupply.com","script")
+def getPrefix(url, tags):
+    r = requests.get(url)
+    s = soup(r.content, 'html.parser')
+    t = s.find_all(tags)
+    u = str(t[2])
+    v = re.findall(r'[a-z]*1', u)[0]
+    return v
+
+
+def getTagFromURL(url, tags):
+    r = requests.get(url)
+    souping = soup(r.content, 'html.parser')
+    return souping.find_all(tags)
+
+
+def getID(url, tags):
+    r = requests.get(url)
+    s = soup(r.content, 'html.parser')
+    t = s.find_all(tags)
+    u = str(t)
+    v = re.findall(r'\bid\s*:\s(\d{11}),\s*parent_id', u)[0]
+    return v
+
+
+ing2 = getPrefix("http://yeezysupply.com", "script")
 mix1 = ing1 + ing2
-ing3 =str(test5(mix1, "a"))
-mix2= (re.findall(r'/products/[a-z0-9]*',ing3))
+ing3 = str(getTagFromURL(mix1, "a"))
+mix2 = re.findall(r'/products/[a-z0-9]*', ing3)
 fmix2 = mix2[0]
-truemix2= ing0 + fmix2
+truemix2 = ing0 + fmix2
+ing4 = getID(truemix2, "script")
 
-def secondtest3d1(url, tags):
-                r = requests.get(url)
-                s = soup(r.content, 'html.parser')
-                t = s.find_all(tags)
-                u = str(t)
-                v = (re.findall(r'\d{11}', u))
-                return v
-
-
-def secondtest4():  # En tu caso, no le declares una variable a la funcion
-        for item in ing4:
-          if item != ing4[0]:
-              return item
-
-ing4 = secondtest3d1(truemix2, "script")
-ing5 = (re.findall(r'id:/s*/d{11}', ing4))
-mix3 = secondtest4()
-fmix3 = mix3[:11]
-payload = {"quantity": "1", "id": fmix3}
-pming = requests.post(ming, json =payload)
-print (pming.content)
-
-
-
-
-
-
-
-
-     
-     
+payload = {"quantity": "1", "id": ing4}
+pming = requests.post(ming, json=payload)
+print(pming.text)
--- yeezySupply.py
+++ yeezySupply.py
@@ -7,5 +7,49 @@ class YeezySupply:
     name = ""
     customer_info = {}
 
+    ming = "https://yeezysupply.com/cart/add.js"
+    ing0 = "https://yeezysupply.com/"
+    ing1 = "https://yeezysupply.com/collections/"
+
     def __init__(self, cust_info):
         self.customer_info = cust_info
+
+    def getPrefix(self, url, tags):
+        r = requests.get(url)
+        s = soup(r.content, 'html.parser')
+        t = s.find_all(tags)
+        u = str(t[2])
+        v = re.findall(r'[a-z]*1', u)[0]
+        return v
+
+    def getTagFromURL(self, url, tags):
+        r = requests.get(url)
+        souping = soup(r.content, 'html.parser')
+        return souping.find_all(tags)
+
+    def getID(self, url, tags):
+        r = requests.get(url)
+        s = soup(r.content, 'html.parser')
+        t = s.find_all(tags)
+        u = str(t)
+        v = re.findall(r'\bid\s*:\s(\d{11}),\s*parent_id', u)[0]
+        return v
+
+    def add_to_cart(self, url):
+        ing2 = self.getPrefix("http://yeezysupply.com", "script")
+        mix1 = self.ing1 + ing2
+        ing3 = str(self.getTagFromURL(mix1, "a"))
+        mix2 = re.findall(r'/products/[a-z0-9]*', ing3)
+        fmix2 = mix2[0]
+        truemix2 = self.ing0 + fmix2
+        ing4 = self.getID(truemix2, "script")
+
+        payload = {"quantity": "1", "id": ing4}
+        pming = requests.post(self.ming, json=payload)
+        print(pming.text)
+
+if __name__ == '__main__':
+    bot = YeezySupply({})
+
+    bot.add_to_cart("")
+
